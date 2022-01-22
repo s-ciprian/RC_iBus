543eb5d9b8d5132d28d3d8f0689ff47b83873252
Ciprian Salageanu <cipriansalageanu@gmail.com>
2022-01-22 14:55:35 +0200

    First commit. Empty application within a project generated by STM32CubeIDE.

---
+++ Appl/ApplMain/applMain.cpp
@@ -0,0 +1,30 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stdint.h"
+
+#include "applMain.h"
+
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t mainCnt;
+
+/**
+  * @brief Main application - initialization function.
+  * @retval None
+  */
+void mainAppl_Init(void)
+{
+	mainCnt = 0u;
+
+	/* TODO: Add implementation here */
+}
+
+/**
+  * @brief Main application entry point. 
+  * @retval None
+  */
+void mainAppl_MainRunnable(void)
+{
+	mainCnt++;
+
+	/* TODO: Add implementation here */
+}
---
+++ Appl/ApplMain/applMain.h
@@ -0,0 +1,19 @@
+#ifndef APPL_MAIN_H
+#define APPL_MAIN_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+/* Exported functions */
+void mainAppl_Init(void);
+void mainAppl_MainRunnable(void);
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
+
+#endif
